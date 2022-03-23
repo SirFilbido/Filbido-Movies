7b1ef5ae477ff51eebddac6fa0bc650275f1e2dd
Osmar Filbido de Andrade Filho <osmar.filbido@gmail.com>
2022-03-23 11:06:25 -0300

    Folder organization and renaming from Main to Home

---
+++ app/src/main/java/com/sirfilbido/filbidomovies/app/App.kt
@@ -0,0 +1,7 @@
+package com.sirfilbido.filbidomovies.app
+
+import android.app.Application
+
+class App : Application(){
+    
+}
--- app/src/main/java/com/sirfilbido/filbidomovies/presentation/MainActivity.kt
+++ app/src/main/java/com/sirfilbido/filbidomovies/presentation/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.sirfilbido.filbidomovies
+package com.sirfilbido.filbidomovies.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.sirfilbido.filbidomovies.ui.main.MainFragment
+import com.sirfilbido.filbidomovies.R
+import com.sirfilbido.filbidomovies.presentation.ui.main.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(R.id.container, HomeFragment.newInstance())
                 .commitNow()
         }
     }
