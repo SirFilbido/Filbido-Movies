9c9dd1859d628c931d47e0b97d9e8e3426ee6827
Osmar Filbido de Andrade Filho <osmar.filbido@gmail.com>
2022-03-23 16:27:52 -0300

    Setting DomainModule and adding AppModule

--- app/src/main/java/com/sirfilbido/filbidomovies/app/AppModule.kt
+++ app/src/main/java/com/sirfilbido/filbidomovies/app/AppModule.kt
@@ -1,10 +1,12 @@
 package com.sirfilbido.filbidomovies.app
 
 import com.sirfilbido.filbidomovies.data.di.dataModule
+import com.sirfilbido.filbidomovies.domain.di.domainModule
 import com.sirfilbido.filbidomovies.presentation.di.presentationModule
 import org.koin.core.module.Module
 
 fun appModule(): List<Module> = arrayListOf<Module>().apply {
     addAll(dataModule())
+    addAll(domainModule())
     addAll(presentationModule())
 }
--- app/src/main/java/com/sirfilbido/filbidomovies/domain/di/DomainModule.kt
+++ app/src/main/java/com/sirfilbido/filbidomovies/domain/di/DomainModule.kt
@@ -1,2 +1,12 @@
 package com.sirfilbido.filbidomovies.domain.di
 
+import com.sirfilbido.filbidomovies.domain.interactor.GetListNowPlayingUseCase
+import org.koin.dsl.module
+
+fun domainModule() = arrayListOf(
+    useCaseModule(),
+)
+
+private fun useCaseModule() = module {
+    factory { GetListNowPlayingUseCase(get()) }
+}
