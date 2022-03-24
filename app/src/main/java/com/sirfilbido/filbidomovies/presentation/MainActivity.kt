d2dbd1a2319131798af348a4e82d6844cc42f154
Osmar Filbido de Andrade Filho <osmar.filbido@gmail.com>
2022-03-24 16:36:49 -0300

    Adding class State and UseCase in ViewModel

--- app/src/main/java/com/sirfilbido/filbidomovies/presentation/MainActivity.kt
+++ app/src/main/java/com/sirfilbido/filbidomovies/presentation/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.sirfilbido.filbidomovies.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.sirfilbido.filbidomovies.R
-import com.sirfilbido.filbidomovies.presentation.ui.main.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, HomeFragment.newInstance())
-                .commitNow()
-        }
+        setContentView(R.layout.home_activity)
     }
 }
--- app/src/main/java/com/sirfilbido/filbidomovies/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/sirfilbido/filbidomovies/presentation/ui/home/HomeViewModel.kt
@@ -1,7 +1,39 @@
 package com.sirfilbido.filbidomovies.presentation.ui.home
 
+import android.os.RemoteException
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sirfilbido.filbidomovies.data.model.Movie
+import com.sirfilbido.filbidomovies.domain.interactor.movie.GetListNowPlayingUseCase
+import com.sirfilbido.filbidomovies.presentation.State
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel(
+    private val _getListNowPlayingUseCase: GetListNowPlayingUseCase,
+) : ViewModel() {
+
+    private val _listMovie = MutableLiveData<State<List<Movie>>>()
+    val listMovie: LiveData<State<List<Movie>>>
+        get() = _listMovie
+
+    fun fetchMovies() {
+        viewModelScope.launch {
+            _getListNowPlayingUseCase()
+                .onStart {
+                    _listMovie.postValue(State.Loading)
+//                    delay(800)
+                }.catch {
+                    with(RemoteException("Could not connect to API")) {
+                        _listMovie.postValue(State.Error(this))
+                    }
+                }.collect {
+                    _listMovie.postValue(State.Success(it))
+                }
+        }
+    }
 }
