f404f7381d294570504fafe60c106f024e00da2f
Osmar Filbido de Andrade Filho <osmar.filbido@gmail.com>
2022-03-24 19:16:06 -0300

    Refactoring toMovies in GetListNowPlayingUseCase

--- app/src/main/java/com/sirfilbido/filbidomovies/domain/interactor/movie/GetListNowPlayingUseCase.kt
+++ app/src/main/java/com/sirfilbido/filbidomovies/domain/interactor/movie/GetListNowPlayingUseCase.kt
@@ -1,5 +1,7 @@
 package com.sirfilbido.filbidomovies.domain.interactor.movie
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.sirfilbido.filbidomovies.data.model.Genre
 import com.sirfilbido.filbidomovies.data.model.Movie
 import com.sirfilbido.filbidomovies.data.repository.movie.MovieRepository
@@ -7,49 +9,52 @@ import com.sirfilbido.filbidomovies.data.services.movie.response.MovieResponse
 import com.sirfilbido.filbidomovies.domain.interactor.UseCase.NoParam
 import com.sirfilbido.filbidomovies.domain.interactor.genre.GetGenresUseCase
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class GetListNowPlayingUseCase(
     private val _repository: MovieRepository,
     private val _getGenresUseCase: GetGenresUseCase,
 ) : NoParam<List<Movie>>() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override suspend fun execute(): Flow<List<Movie>> {
-        val movies = _repository.getListNowPlaying()
-        val genres = _getGenresUseCase.execute()
+        val movies = _repository.getListNowPlaying().first()
+        val genres = _getGenresUseCase.execute().first()
         return toMovies(movies, genres)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun toMovies(
-        movies: Flow<List<MovieResponse>>,
-        genres: Flow<List<Genre>>,
+        movies: List<MovieResponse>,
+        genres: List<Genre>,
     ): Flow<List<Movie>> {
 
         val list = mutableListOf<Movie>()
 
-        movies.map { listMovies ->
-            listMovies.map { movie ->
+        movies.map { movie ->
 
-                val listGenres = mutableListOf<Genre>()
-                movie.genres.forEach { idGenres ->
-                    genres.map { listGenres.add(it.find { genre -> genre.id == idGenres }!!) }
-                }
-
-                list.add(
-                    Movie(
-                        id = movie.id,
-                        poster = movie.poster,
-                        title = movie.title,
-                        overview = movie.title,
-                        releaseDate = movie,
-                        genres = listGenres
-                    )
-                )
+            val listGenres = mutableListOf<Genre>()
+            movie.genres.forEach { idGenres ->
+                listGenres.add(genres.find { genre -> genre.id == idGenres }!!)
             }
-        }
 
-        println(list)
+            list.add(
+                Movie(
+                    id = movie.id,
+                    poster = movie.poster,
+                    title = movie.title,
+                    overview = movie.overview,
+                    releaseDate = LocalDate.parse(
+                        movie.releaseDate,
+                        DateTimeFormatter.ISO_DATE
+                    ),
+                    genres = listGenres
+                )
+            )
+        }
 
         return flow { emit(list) }
     }
