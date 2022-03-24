fe5ae34bae64a328ed39777f460a351481240a81
Osmar Filbido de Andrade Filho <osmar.filbido@gmail.com>
2022-03-24 19:14:14 -0300

    Adding UI files, itemMovie and navigation

--- app/src/main/java/com/sirfilbido/filbidomovies/data/services/movie/response/MovieResponse.kt
+++ app/src/main/java/com/sirfilbido/filbidomovies/data/services/movie/response/MovieResponse.kt
@@ -6,7 +6,7 @@ class MovieResponse(
     val id: Long,
     @Json(name = "poster_path") val poster: String,
     val title: String,
-    val releaseDate: String,
+    @Json(name = "release_date") val releaseDate: String,
     val overview: String,
     @Json(name = "genre_ids") val genres: List<Long>,
 )
